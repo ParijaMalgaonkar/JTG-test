0152547129e0bced9c18262260311313c629b090
Parija Malgaonkar <parijamalgaonkar@gmail.com>
2021-08-16 17:11:03 +0530

    css changes

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,1 +1,17 @@
-var pagination = document.getElementsByClassName('pagination');
+const navSlide = () => {
+    const hamburger = document.querySelector('.hamburger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelector('.nav-links li');
+
+    hamburger.addEventListener('click', ()=> {
+        nav.classList.toggle('nav-active');
+    });
+
+    navLinks.forEach((link, index) => {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7}s`;
+    });
+
+
+}
+
+navSlide();
